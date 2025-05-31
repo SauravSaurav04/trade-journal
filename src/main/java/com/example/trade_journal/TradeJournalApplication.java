c3983bc892d76f898c9f92c196737ba765a623a6
SauravSaurav04 <sauravsaurav4444@gmail.com>
2025-06-01 00:11:45 +0530

    first commit

---
+++ src/main/java/com/example/trade_journal/TradeJournalApplication.java
@@ -0,0 +1,13 @@
+package com.example.trade_journal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeJournalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/trade_journal/TradeJournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trade_journal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeJournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
